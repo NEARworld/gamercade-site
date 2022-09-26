d1df961cbbbe175d071cafbcdd671c23b168f2c2
NEARworld <artemismars2@outlook.com>
2022-09-26 13:29:30 +0900

    fix: resolved import paths issue

---
+++ src/components/navbar/Logo.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function Logo() {
+  return <LogoWrapper>
+    <img src="" />
+    <h1>Gamercade</h1>
+  </LogoWrapper>
+}
+export default Logo;
+
+const LogoWrapper = styled.div`
+  
+`
