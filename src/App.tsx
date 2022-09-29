1e6672cae487168187ebd9ee9c28287afa2356c0
NEARworld <artemismars2@outlook.com>
2022-09-29 16:34:42 +0900

    style: global style and logo image

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from 'components/navbar';
 import Main from 'pages/main';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GlobalStyle from 'styles/GlobalStyle';
 
 function App() {
   return <Router>
+    <GlobalStyle />
     <Navbar />
     <Routes>
       <Route path='/' element={<Main />} />
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Ubuntu', sans-serif;
+  }
+`
+export default GlobalStyle;
