eb009fdc5138ab04262432d2d8ebbd392db0e97d
NEARworld <artemismars2@outlook.com>
2022-10-04 15:37:42 +0900

    fix(Main): update imports and remove unncecessary codes

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,5 +1,6 @@
-import Card, { cards } from 'components/Card';
+import GameList from 'components/gamelist';
 import Label from 'components/Label';
+import { labels } from 'mocks/labels';
 import styled from 'styled-components';
 
 export interface ILabel {
@@ -9,45 +10,6 @@ export interface ILabel {
   text: string;
 };
 
-export const labels = [
-  {
-    id: 1,
-    bg: '#3472AA',
-    stroke: '#4A90D0',
-    text: 'cyberpunk'
-  },
-  {
-    id: 2,
-    bg: '#AA3434',
-    stroke: '#F54E4E',
-    text: 'action'
-  },
-  {
-    id: 3,
-    bg: '#AA6634',
-    stroke: '#DC8C52',
-    text: 'RPG'
-  },
-  {
-    id: 4,
-    bg: '#623799',
-    stroke: '#9155DD',
-    text: 'fantasy'
-  },
-  {
-    id: 5,
-    bg: '#AA5134',
-    stroke: '#ED7750',
-    text: 'history'
-  },
-  {
-    id: 6,
-    bg: '#34AA72',
-    stroke: '#61E8A8',
-    text: 'shooting'
-  }
-]
-
 function Main() {
   return <MainWrapper>
     <LabelWrapper>
@@ -55,10 +17,7 @@ function Main() {
         labels.map((item) => <Label key={item.id} bg={item.bg} stroke={item.stroke} text={item.text} />)
       }
     </LabelWrapper>
-    <GameList>
-      {cards.map((item) => <Card name={item.name} images={item.images} labels={item.tags} />)}
-      {Array.from({ length: 30 }).map(() => <Card name={''} images={{ main: '', sub: [''] }} />)}
-    </GameList>
+    <GameList />
   </MainWrapper>
 }
 export default Main;
@@ -72,10 +31,4 @@ const LabelWrapper = styled.div`
   display: flex;
   gap: 10px;
   justify-content: center;
-`
-const GameList = styled.div`
-  display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  gap: 30px;
-  margin-top: 50px;
 `
