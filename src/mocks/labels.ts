bae9bf76dce53020124288d6af7212269529ae46
NEARworld <artemismars2@outlook.com>
2022-10-04 15:36:35 +0900

    feat(mocks): cards and labels in mocks directory

---
+++ src/mocks/labels.ts
@@ -0,0 +1,38 @@
+export const labels = [
+    {
+      id: 1,
+      bg: '#3472AA',
+      stroke: '#4A90D0',
+      text: 'cyberpunk'
+    },
+    {
+      id: 2,
+      bg: '#AA3434',
+      stroke: '#F54E4E',
+      text: 'action'
+    },
+    {
+      id: 3,
+      bg: '#AA6634',
+      stroke: '#DC8C52',
+      text: 'RPG'
+    },
+    {
+      id: 4,
+      bg: '#623799',
+      stroke: '#9155DD',
+      text: 'fantasy'
+    },
+    {
+      id: 5,
+      bg: '#AA5134',
+      stroke: '#ED7750',
+      text: 'history'
+    },
+    {
+      id: 6,
+      bg: '#34AA72',
+      stroke: '#61E8A8',
+      text: 'shooting'
+    }
+  ]
