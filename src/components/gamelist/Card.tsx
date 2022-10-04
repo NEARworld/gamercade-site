d35ed0024f2064def999ce362c7b341aeb4bded8
NEARworld <artemismars2@outlook.com>
2022-10-04 15:35:41 +0900

    fix: create gamelist and  move Card into gamelist

--- src/components/gamelist/Card.tsx
+++ src/components/gamelist/Card.tsx
@@ -1,30 +1,6 @@
 import { labels } from "pages/main";
 import styled from "styled-components";
-import Label from "./Label";
-
-export const cards = [
-  {
-    id: 1,
-    name: 'Cyberpunk 2077',
-    description: 'The dark future in 2077',
-    tags: ['action', 'cyberpunk'],
-    developers: ['Ian', 'Joe', 'Jack', 'Smith'],
-    images: {
-      main: 'https://i.pinimg.com/originals/9d/80/aa/9d80aafb807665611800cf6182a90790.jpg',
-      sub: []
-    }
-  }, {
-    id: 2,
-    name: 'Fantasy Saga',
-    description: 'The Magic',
-    tags: ['fantasy'],
-    developers: ['Joe'],
-    images: {
-      main: 'https://i.pinimg.com/originals/59/80/c2/5980c2f21b19972ca2346a9037f66493.png',
-      sub: []
-    }
-  }
-]
+import Label from 'components/Label';
 
 interface Images {
   main: string;
