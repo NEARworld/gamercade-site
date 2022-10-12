ea2d5483908106a4baa91a3dd857b90ea755f564
NEARworld <artemismars2@outlook.com>
2022-10-12 19:40:40 +0900

    feat(modal): adding left and right into Modal component

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -2,6 +2,8 @@ import { modalStatusType } from "hooks/useModal";
 import { cards } from "mocks/cards";
 import { useEffect, useState } from "react";
 import styled from "styled-components"
+import Left from "./Left";
+import Right from "./Right";
 
 interface Props {
   modalStatus: modalStatusType; 
@@ -10,35 +12,36 @@ interface Props {
 function Modal({modalStatus}: Props) {
   const card = cards.find((item) => item.id === modalStatus.id)
   return  <Wrapper>
-    <ModalBody onMouseDown={(e) => {
-      e.nativeEvent.stopImmediatePropagation()}
-    }>
-      {card ? 
-        <Title>{card.name}</Title> 
-        
-        : null}
-    </ModalBody>
+    {card ? 
+      <ModalBody onMouseDown={(e) => {
+        e.nativeEvent.stopImmediatePropagation()}
+      }>
+          <Left images={card.images} />
+          <Right />
+      </ModalBody>
+      : null}
   </Wrapper>
 }
 
 export default Modal;
 const Wrapper = styled.div`
-  position: relative;
-  height: 100%;
-  `
-const ModalBody = styled.div`
   position: fixed;
-  margin: 0 auto;
-  transform: translateY(50%);
-  left: 0;
-  right: 0;
-  width: 500px;
-  aspect-ratio: 1;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const ModalBody = styled.div`
+  max-width: 80%;
   text-align: center;
   background-color: ${props => props.theme.dark.bg.primary};
   color: white;
   border: 1px solid ${props => props.theme.dark.border.primary};
   border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding: 30px;
 `
 const Title = styled.h4`
 `
