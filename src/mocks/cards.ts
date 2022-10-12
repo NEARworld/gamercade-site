4c4e0c0c78d5e0a196ad8d125aebfd5075f40425
NEARworld <artemismars2@outlook.com>
2022-10-12 19:37:29 +0900

    fix(mocks): update cards for carousel test

--- src/mocks/cards.ts
+++ src/mocks/cards.ts
@@ -17,18 +17,26 @@ export const cards = [
       developers: ['Joe'],
       images: {
         main: 'https://i.pinimg.com/originals/59/80/c2/5980c2f21b19972ca2346a9037f66493.png',
-        sub: []
+        sub: ['https://i.pinimg.com/originals/1e/bb/23/1ebb23ea94e4f885d37446c70be52145.png',
+        'https://img.itch.zone/aW1nLzEzMDE5NDgucG5n/original/oGPg5H.png',
+      ]
       }
     },
     {
-      id: 2,
-      name: 'Fantasy Saga',
+      id: 3,
+      name: 'Wonderland',
       description: 'The Magic',
       tags: ['fantasy'],
       developers: ['Joe'],
       images: {
-        main: 'https://i.pinimg.com/originals/59/80/c2/5980c2f21b19972ca2346a9037f66493.png',
-        sub: []
+        main: 'https://pbs.twimg.com/media/DhIHXP5UwAAEXi6.png',
+        sub: ['https://i.pinimg.com/originals/1e/bb/23/1ebb23ea94e4f885d37446c70be52145.png',
+        'https://img.itch.zone/aW1nLzEzMDE5NDgucG5n/original/oGPg5H.png',
+        'https://cdnb.artstation.com/p/assets/images/images/032/387/895/large/philipp-a-urlich-bridge-320x240-des.jpg?1606294898',
+        'https://pbs.twimg.com/media/DhIHXP5UwAAEXi6.png',
+        'https://cdn.mos.cms.futurecdn.net/EFXSes9UCfsyRVoNeQ2ZTB-1200-80.png',
+        'https://preview.redd.it/jt244nc9rjf51.png?auto=webp&s=e5a8a1b812ce2b205e77283495c7fd3632f69cc0'
+      ]
       }
     },
     {
