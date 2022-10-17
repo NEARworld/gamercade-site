7e71f8fbac3e6bd80cbd66deb07ac1871de833c8
NEARworld <artemismars2@outlook.com>
2022-10-17 17:58:05 +0900

    style(global): change GlobalStyle filename to global

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import Navbar from 'components/navbar';
-import SideMenu from 'components/navbar/SideMenu';
-import Main from 'pages/main';
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyle, { theme } from 'styles/GlobalStyle';
+import Navbar from "components/navbar";
+import SideMenu from "components/navbar/SideMenu";
+import Main from "pages/main";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle, { theme } from "styles/global";
 
 function App() {
   const [isSideOpen, setIsSideOpen] = useState(false);
-  return <Router>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Navbar setIsSideOpen={setIsSideOpen} />
-      <SideMenu isSideOpen={isSideOpen} setIsSideOpen={setIsSideOpen} />
-      <Routes>
-        <Route path='/' element={<Main />} />
-      </Routes>
-    </ThemeProvider>
-  </Router>
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Navbar setIsSideOpen={setIsSideOpen} />
+        <SideMenu isSideOpen={isSideOpen} setIsSideOpen={setIsSideOpen} />
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </ThemeProvider>
+    </Router>
+  );
 }
 
 export default App;
