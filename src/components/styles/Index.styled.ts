8cb0db749a3153e4455b4582306a0217e386ce7a
NEARworld <artemismars2@outlook.com>
2022-10-20 19:34:39 +0900

    fix(gamelist): separate styled components from GameList component

---
+++ src/components/styles/Index.styled.ts
@@ -0,0 +1,8 @@
+import {GameList, CardObj} from 'components/styles/GameList.styled';
+import {Arrow} from 'components/styles/Arrow.styled';
+const Styled = {
+  GameList,
+  CardObj,
+  Arrow
+};
+export default Styled;
