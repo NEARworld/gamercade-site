027830301b2e352ab6faeef2e4d4dcb36bc47793
NEARworld <artemismars2@outlook.com>
2022-10-22 18:58:43 +0900

    feat(modal): implementing the right side

--- src/components/modal/Right.tsx
+++ src/components/modal/Right.tsx
@@ -1,36 +1,80 @@
+import { getLabel } from "components/Label";
 import { type ICard } from "mocks/cards";
-import styled from "styled-components"
+import styled from "styled-components";
 import { devices } from "styles/global";
 
 interface Props {
   card: ICard<string>;
 }
-
-function Right({card}: Props) {
-  return <Wrapper>
-    <Title>{card.name}</Title>
-    <ContentContainer>
+function Right({ card }: Props) {
+  const cardKeys = Object.keys(card);
+  const validKeys = [
+    "developers",
+    "release",
+    "description",
+    "multiplayer",
+    "singleplayer",
+    "languages",
+    "website",
+    "tags",
+    "download",
+  ];
+  function filterContent(key: string) {
+    if (!validKeys.includes(key)) return;
+    if (key === "developers" || key === "languages")
+      return (
+        <Row>
+          <Item>{key}</Item>
+          <Detail>{card[key].join(", ")}</Detail>
+        </Row>
+      );
+    if (key === "tags")
+      return (
+        <Row>
+          <Item>{key}</Item>
+          <Detail>{card[key].map((label) => getLabel(label))}</Detail>
+        </Row>
+      );
+    return (
       <Row>
-        <Avatar avatar={card.avatar}/>
-        <AuthorWrapper>
-          <Name>{card.author}</Name>
-          <PublishDate>{card.date}</PublishDate>
-        </AuthorWrapper>
+        <Item>{key}</Item>
+        <Detail>{card[key]}</Detail>
       </Row>
-      {Object.keys(card).map((item) => {
-        if (item === 'id' || 
-            item === 'name'  || 
-            item === 'author' || 
-            item === 'date' || 
-            item === 'images'  || 
-            item === 'avatar') return;
-          return (<Row>
-          <Item>{item}</Item>
-          <Detail>{card[item]}</Detail>
-        </Row>)
-      })}
-    </ContentContainer>
-  </Wrapper>
+    );
+  }
+  // function handleArray(key: string) {
+  //   if (key === "developers" || key === "languages") {
+  //     return card[key].join(", ");
+  //   }
+  // }
+  // function handleLabel(key: string) {
+  //   if (key === "tags") return card[key].map((label) => getLabel(label));
+  // }
+
+  // function pipe(...fns: Function[]) {
+  //   return function pipled(data: string) {
+  //     for (let fn of fns) {
+  //       data = fn(data);
+  //     }
+  //     console.log(data);
+  //     return <Detail>{data}</Detail>;
+  //   };
+  // }
+  return (
+    <Wrapper>
+      <Title>{card.name}</Title>
+      <ContentContainer>
+        <Row>
+          <Avatar avatar={card.avatar} />
+          <AuthorWrapper>
+            <Name>{card.author}</Name>
+            <PublishDate>{card.date}</PublishDate>
+          </AuthorWrapper>
+        </Row>
+        {cardKeys.map((key) => filterContent(key))}
+      </ContentContainer>
+    </Wrapper>
+  );
 }
 export default Right;
 
@@ -41,12 +85,11 @@ const Wrapper = styled.div`
   @media ${devices.laptop} {
     display: block;
   }
-`
+`;
 const Title = styled.h4`
   margin-top: 0;
-`
-const ContentContainer = styled.div`
-`
+`;
+const ContentContainer = styled.div``;
 const Row = styled.div`
   display: grid;
   grid-template-columns: 1fr 4fr;
@@ -55,31 +98,31 @@ const Row = styled.div`
   }
   align-items: top;
   margin-bottom: 10px;
-`
+`;
 const Item = styled.p`
   margin: 0;
   text-align: left;
   font-weight: bold;
-`
+`;
 const Detail = styled.div`
+  display: flex;
+  gap: 5px;
   text-align: left;
   max-width: 350px;
   line-break: anywhere;
-`
-const Avatar = styled.div<{avatar: string}>`
+`;
+const Avatar = styled.div<{ avatar: string }>`
   display: inline-block;
   width: 50px;
   aspect-ratio: 1;
-  background-image: url(${props => props.avatar});
+  background-image: url(${(props) => props.avatar});
   background-position: center;
   background-size: cover;
   border-radius: 50%;
-`
+`;
 const AuthorWrapper = styled.div`
   text-align: left;
-  color: #4C85A5;
-`
-const Name = styled.p`
-`
-const PublishDate = styled.p`
-`
+  color: #4c85a5;
+`;
+const Name = styled.p``;
+const PublishDate = styled.p``;
